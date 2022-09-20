8655e67d36637d23396bf37d1f554942c755808c
shiyuhang <1136742008@qq.com>
2022-09-20 17:54:08 +0800

    update doc

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,4 +1,17 @@
+# We provide a full example in Readme https://github.com/tidbcloud/terraform-provider-tidbcloud
+
+terraform {
+  required_providers {
+    tidbcloud = {
+      source  = "tidbcloud/tidbcloud"
+      version = "~> 0.0.1"
+    }
+  }
+  required_version = ">= 1.0.0"
+}
+
 provider "tidbcloud" {
   username = "fake_username"
   password = "fake_password"
 }
+
