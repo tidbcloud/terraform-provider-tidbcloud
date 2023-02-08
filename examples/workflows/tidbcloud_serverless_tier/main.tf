babbe7a414a797e6674340c4e53e677d3978d759
shi yuhang <52435083+shiyuhang0@users.noreply.github.com>
2023-02-08 14:55:54 +0800

    Add workflow for terraform (#56)
    Add workflow for terraform

---
+++ examples/workflows/tidbcloud_serverless_tier/main.tf
@@ -0,0 +1,46 @@
+variable "cluster_name" {
+  type     = string
+  nullable = false
+}
+
+variable "cloud_provider_region" {
+  type     = string
+  nullable = false
+  default  = "us-east-1"
+}
+
+variable "password" {
+  type      = string
+  nullable  = false
+  sensitive = true
+}
+
+terraform {
+  required_providers {
+    tidbcloud = {
+      source = "tidbcloud/tidbcloud"
+    }
+  }
+}
+
+provider "tidbcloud" {
+  # export TIDBCLOUD_PUBLIC_KEY and TIDBCLOUD_PRIVATE_KEY with the TiDB Cloud API Key
+}
+
+data "tidbcloud_projects" "projects" {
+}
+
+resource "tidbcloud_cluster" "example" {
+  project_id     = element(data.tidbcloud_projects.projects.items, 0).id
+  name           = var.cluster_name
+  cluster_type   = "DEVELOPER"
+  cloud_provider = "AWS"
+  region         = var.cloud_provider_region
+  config = {
+    root_password = var.password
+  }
+}
+
+output "connection_strings" {
+  value = lookup(tidbcloud_cluster.example.status, "connection_strings")
+}
