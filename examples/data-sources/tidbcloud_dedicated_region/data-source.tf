52ea1a240481c0528beed6a520a81c7216bffd1a
FingerLeader <wanxfinger@gmail.com>
2024-12-09 00:45:23 +0800

    add examples
    Signed-off-by: FingerLeader <wanxfinger@gmail.com>

---
+++ examples/data-sources/tidbcloud_dedicated_region/data-source.tf
@@ -0,0 +1,11 @@
+variable "region_id" {
+  type     = string
+}
+
+data "tidbcloud_dedicated_region" "example" {
+  region_id = var.region_id
+}
+
+output "output" {
+  value = data.tidbcloud_dedicated_region.example
+}
---
+++ examples/data-sources/tidbcloud_dedicated_regions/data-source.tf
@@ -0,0 +1,18 @@
+variable "project_id" {
+  type     = string
+  nullable = true
+}
+
+variable "cloud_provider" {
+  type     = string
+  nullable = true
+}
+
+data "tidbcloud_dedicated_regions" "example" {
+  project_id = var.project_id
+  cloud_provider = var.cloud_provider
+}
+
+output "output" {
+  value = data.tidbcloud_dedicated_regions.example
+}
