2968e1e8f8188f5f88c068e63dc3243584a128d9
FingerLeader <wanxfinger@gmail.com>
2025-01-15 14:53:56 +0800

    add example
    Signed-off-by: FingerLeader <wanxfinger@gmail.com>

---
+++ examples/data-sources/tidbcloud_dedicated_node_groups/data-source.tf
@@ -0,0 +1,12 @@
+variable "cluster_id" {
+  type     = string
+  nullable = false
+}
+
+data "tidbcloud_dedicated_node_groups" "example" {
+  cluster_id     = var.cluster_id
+}
+
+output "output" {
+  value = data.tidbcloud_dedicated_node_groups.example
+}
---
+++ examples/resources/tidbcloud_dedicated_cluster/resource.tf
@@ -0,0 +1,31 @@
+variable "name" {
+  type     = string
+  nullable = false
+}
+
+variable "region_id" {
+  type     = string
+  nullable = false
+}
+
+variable "root_password" {
+  type     = string
+  nullable = false
+}
+
+resource "tidbcloud_dedicated_cluster" "example" {
+    name = var.name
+    region_id = var.region_id
+    port = 4000
+    root_password = var.root_password
+    tidb_node_setting = {
+      node_spec_key = "2C4G"
+      node_count = 1
+    }
+    tikv_node_setting = {
+      node_spec_key = "2C4G"
+      node_count = 3
+      storage_size_gi = 10
+      storage_type = "BASIC"
+    }
+}
